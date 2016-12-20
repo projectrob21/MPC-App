29fc247a0d10f6a0c2a4317b4e97fd982a195de7
Rob <robdeans@gmail.com>
2016-12-20 17:25:08 -0500

    Added all files to date
    adds all the work from previous commits

---
+++ MPCApp/MPCApp/MPCView.swift
@@ -0,0 +1,132 @@
+//
+//  Pad View.swift
+//  MPCApp
+//
+//  Created by Robert Deans on 12/18/16.
+//  Copyright © 2016 Robert Deans. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+class MPCView: UIView {
+    
+    var padButton1: PadButton!
+    var padButton2: PadButton!
+    var padButton3: PadButton!
+    
+    var padView1: PadView!
+    var padView2: PadView!
+    var padView3: PadView!
+
+    var padButtonArray = [PadButton]()
+    var padViewArray = [PadView]()
+    
+    var recordButton: UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    convenience init() {
+        self.init(frame: CGRect.zero)
+        configure()
+        constrain()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        padButton1 = PadButton()
+        padButton2 = PadButton()
+        padButton3 = PadButton()
+        
+        padView1 = PadView()
+        padView2 = PadView()
+        padView3 = PadView()
+        
+        padButtonArray = [padButton1, padButton2, padButton3]
+        padViewArray = [padView1, padView2, padView3]
+        
+        var padViewTag = 0
+        for view in padViewArray {
+            padViewTag += 1
+            view.tag = padViewTag
+            view.backgroundColor = UIColor.orange
+            view.layer.cornerRadius = 25
+        }
+
+        var padButtonTag = 0
+        for button in padButtonArray {
+            padButtonTag += 1
+            button.tag = padButtonTag
+            button.layer.cornerRadius = 20
+            button.backgroundColor = UIColor.cyan
+        }
+
+        
+        recordButton = UIButton()
+        recordButton.titleLabel?.text = "Record"
+        recordButton.titleLabel?.textColor = UIColor.white
+        recordButton.backgroundColor = UIColor.red.withAlphaComponent(0.5)
+
+    }
+    
+    func constrain() {
+        
+        addSubview(padView1)
+        padView1.snp.makeConstraints() {
+            $0.topMargin.equalToSuperview().offset(20)
+            $0.leading.equalToSuperview().offset(20)
+            $0.width.equalToSuperview().dividedBy(4)
+            $0.height.equalTo(padView1.snp.width)
+        }
+        
+        padView1.addSubview(padButton1)
+        padButton1.snp.makeConstraints {
+            $0.edges.equalTo(UIEdgeInsetsMake(5, 5, 5, 5))
+        }
+        
+        addSubview(padView2)
+        padView2.snp.makeConstraints {
+            $0.topMargin.equalToSuperview().offset(20)
+            $0.centerX.equalToSuperview()
+            $0.width.equalToSuperview().dividedBy(4)
+            $0.height.equalTo(padView2.snp.width)
+        }
+        
+        padView2.addSubview(padButton2)
+        padButton2.snp.makeConstraints {
+            $0.edges.equalTo(UIEdgeInsetsMake(5, 5, 5, 5))
+        }
+
+        addSubview(padView3)
+        padView3.snp.makeConstraints {
+            $0.topMargin.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.width.equalToSuperview().dividedBy(4)
+            $0.height.equalTo(padView3.snp.width)
+        }
+        
+        padView3.addSubview(padButton3)
+        padButton3.snp.makeConstraints {
+            $0.edges.equalTo(UIEdgeInsetsMake(5, 5, 5, 5))
+        }
+        
+        addSubview(recordButton)
+        recordButton.snp.makeConstraints {
+            $0.bottomMargin.equalToSuperview().offset(-10)
+            $0.centerX.equalToSuperview()
+            $0.width.equalToSuperview().dividedBy(2)
+            $0.height.equalToSuperview().dividedBy(10)
+        }
+        
+    }
+    
+    
+    
+}
---
+++ MPCApp/MPCApp/MPCViewController.swift
@@ -0,0 +1,138 @@
+//
+//  MPCViewController.swift
+//
+//
+//  Created by Robert Deans on 12/18/16.
+//
+//
+
+import UIKit
+import AVFoundation
+
+class MPCViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate, UIGestureRecognizerDelegate {
+    
+    var mpcView: MPCView!
+    
+    var audioRecorder: AVAudioRecorder!
+    var audioPlayer: AVAudioPlayer!
+    var recordingSession: AVAudioSession!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configure()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Set up ViewController
+    func configure() {
+        mpcView = MPCView()
+        view.addSubview(mpcView)
+        mpcView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        assignGestureRecognizers()
+        assignButtonTargets()
+        
+        mpcView.recordButton.addTarget(self, action: #selector(recordAudio(_:)), for: .touchUpInside)
+    }
+    
+    func assignGestureRecognizers() {
+        
+        let recognizer1 = UILongPressGestureRecognizer(target: self, action: #selector(setupRecorder(_:)))
+        recognizer1.delegate = self
+        mpcView.padView1.addGestureRecognizer(recognizer1)
+            
+        let recognizer2 = UILongPressGestureRecognizer(target: self, action: #selector(setupRecorder(_:)))
+        recognizer2.delegate = self
+        mpcView.padView2.addGestureRecognizer(recognizer2)
+        
+        let recognizer3 = UILongPressGestureRecognizer(target: self, action: #selector(setupRecorder(_:)))
+        recognizer3.delegate = self
+        mpcView.padView3.addGestureRecognizer(recognizer3)
+    }
+    
+    func assignButtonTargets() {
+        for padButton in mpcView.padButtonArray {
+            padButton.addTarget(self, action: #selector(padTapped(_:)), for: .touchUpInside)
+        }
+        
+    }
+    
+    func setupRecorder(_ sender: UILongPressGestureRecognizer) {
+        for view in mpcView.padViewArray {
+            view.backgroundColor = UIColor.orange
+        }
+        sender.view?.backgroundColor = UIColor.green
+        
+        guard let padViewTag = sender.view?.tag else { print("error uwnrapping view tag"); return }
+        let audioFile = "padButton\(padViewTag).audioFile.m4a"
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        let audioFilename = documentsDirectory.appendingPathComponent(audioFile)
+        
+        let settings = [AVFormatIDKey : Int(kAudioFormatAppleLossless),
+                        AVEncoderAudioQualityKey : AVAudioQuality.max.rawValue,
+                        AVEncoderBitRateKey : 320000,
+                        AVNumberOfChannelsKey: 2,
+                        AVSampleRateKey: 44100.0 ] as [String : Any]
+        
+        do {
+            audioRecorder =  try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.prepareToRecord()
+            print("ready to record at \(audioFilename)")
+        } catch {
+            if let err = error as Error? {
+                print("AVAudioRecorder error: \(err.localizedDescription)")
+                audioRecorder = nil
+            }
+        }
+    }
+
+    func recordAudio(_ sender: UIButton) {
+        if sender.titleLabel?.text == "Record" {
+            audioRecorder.record()
+            sender.setTitle("Stop", for: .normal)
+        } else {
+            audioRecorder.stop()
+            sender.setTitle("Record", for: .normal)
+        }
+    }
+    
+    func padTapped(_ padButton: UIButton) {
+        preparePlayer(for: padButton.tag)
+        audioPlayer.play()
+    }
+    
+    func preparePlayer(for padButtonTag: Int) {
+        let audioFile = "padButton\(padButtonTag).audioFile.m4a"
+        print("playing audio from \(getFileURL(audioFile))")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: getFileURL(audioFile))
+            audioPlayer.delegate = self
+            audioPlayer.volume = 1.0
+        } catch {
+            if let err = error as Error? {
+                print("AVAudioPlayer error: \(err.localizedDescription)")
+                audioPlayer = nil
+            }
+        }
+    }
+    
+    func getFileURL(_ fileName: String) -> URL {
+        
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = urls[0] as URL
+        let soundURL = documentDirectory.appendingPathComponent(fileName)
+        return soundURL
+    }
+    
+}
